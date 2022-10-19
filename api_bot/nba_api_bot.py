7f87bf3e2954a01c095ed19830b1a82fcf88978c
Ilya Steblyan <102170248+Hrushon@users.noreply.github.com>
2022-10-19 10:01:15 +0600

    Update nba_api_bot.py

--- api_bot/nba_api_bot.py
+++ api_bot/nba_api_bot.py
@@ -1,7 +1,10 @@
-"""Телеграмм-бот для просмотра статистики NBA."""
-
+"""Телеграм-бот для просмотра статистики NBA."""
+import requests
 
 
 ENDPOINT = 'https://www.balldontlie.io/api/v1/'
+CHAT_ID = 2079808924 # Айди аккаунта в телеграм
+BOT_TOKEN = 5562312403:AAFkjYcAtP5Rg0hsXpQtvh9UriqwfrP2KO8 # Токен бота в телеграм
 
-
+response = requests.get(ENDPOINT)
+response = response.json()
