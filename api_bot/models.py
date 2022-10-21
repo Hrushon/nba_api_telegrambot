271caf690de41264ba2697a3ca5e15547451c298
Ilya Steblyan <102170248+Hrushon@users.noreply.github.com>
2022-10-21 10:59:46 +0600

    Update models.py

--- api_bot/models.py
+++ api_bot/models.py
@@ -1,7 +1,5 @@
 """Модели для человекочитаемого вывода информации в ответ на запрос."""
 
-from lib2to3.pgen2.token import PERCENT
-
 
 FOOT_COEFF = 30.48
 INCH_COEFF = 2.54
@@ -124,25 +122,27 @@ def statistics(response):
     games_played = response.get('games_played')
     player_id = response.get('player_id')
     season = response.get('season')
-    min = response.get('min')
-    fgm = response.get('fgm')
-    fga = response.get('fga')
-    fg3m = response.get('fg3m')
-    fg3a = response.get('fg3a')
-    ftm = response.get('ftm')
-    fta = response.get('fta')
-    oreb = response.get('oreb')
-    dreb = response.get('dreb')
-    reb = response.get('reb')
     ast = response.get('ast')
-    stl = response.get('stl')
     blk = response.get('blk')
-    turnover = response.get('turnover')
+    dreb = response.get('dreb')
+    fg3_pct = (response.get('fg3_pct')) * PERC_COEFF
+    fg3a = response.get('fg3a')
+    fg3m = response.get('fg3m')
+    fg_pct = (response.get('fg_pct')) * PERC_COEFF
+    fga = response.get('fga')
+    fgm = response.get('fgm')
+    ft_pct = (response.get('ft_pct')) * PERC_COEFF
+    fta = response.get('fta')
+    ftm = response.get('ftm')
+    min = response.get('min')
+    oreb = response.get('oreb')
     pf = response.get('pf')
     pts = response.get('pts')
-    fg_pct = (response.get('fg_pct')) * PERC_COEFF
-    fg3_pct = (response.get('fg3_pct')) * PERC_COEFF
-    ft_pct = (response.get('ft_pct')) * PERC_COEFF
+    reb = response.get('reb')
+    stl = response.get('stl')
+    turnover = response.get('turnover')
+    if season is not None:
+        season = '{}-{}'.format(season, season + 1)
     statistics_str = (
         'Сезон: {}.\n'
         'Сыгранных игр в сезоне: {}.\n'
@@ -168,3 +168,84 @@ def statistics(response):
     )
     return statistics_str
 
+
+def statistics_game(response):
+    """Модель для сезонной статистики по играм."""
+    id = response.get('id')
+    ast = response.get('ast')
+    blk = response.get('blk')
+    dreb = response.get('dreb')
+    fg3_pct = (response.get('fg3_pct')) * PERC_COEFF
+    fg3a = response.get('fg3a')
+    fg3m = response.get('fg3m')
+    fg_pct = (response.get('fg_pct')) * PERC_COEFF
+    fga = response.get('fga')
+    fgm = response.get('fgm')
+    ft_pct = (response.get('ft_pct')) * PERC_COEFF
+    fta = response.get('fta')
+    ftm = response.get('ftm')
+    game = response.get('game')
+    game_id = game.get('id')
+    game_date = game.get('date')
+    game_home_team_id = game.get('home_team_id')
+    game_home_team_score = game.get('home_team_score')
+    game_season = game.get('season')
+    game_visitor_team_id = game.get('visitor_team_id')
+    game_visitor_team_score = game.get('visitor_team_score')
+    min = response.get('min')
+    oreb = response.get('oreb')
+    pf = response.get('pf')
+    player = response.get('player')
+    player_id = player.get('id')
+    player_first_name = player.get('first_name')
+    player_last_name = player.get('last_name')
+    player_position = player.get('position')
+    player_team_id = player.get('team_id')
+    pts = response.get('pts')
+    reb = response.get('reb')
+    stl = response.get('stl')
+    team = response.get('team')
+    team_id = team.get('id')
+    team_abbreviation = team.get('abbreviation')
+    team_city = team.get('city')
+    team_conference = team.get('conference')
+    team_division = team.get('division')
+    team_full_name = team.get('full_name')
+    team_name = team.get('name')
+    turnover = response.get('turnover')
+    if game_season is not None:
+        game_season = '{}-{}'.format(game_season, game_season + 1)
+    statistics_game_str = (
+        '{} {}\n'
+        'ID игрока: {}\n'
+        'Сезон: {}\n'
+        '{}\n'
+        '{} против {}\n'
+        'Счёт: {}:{}\n'
+        'Средняя статистика за игру по показателям:\n'
+        '+++ набранные очки: {}\n'
+        '+++ сыгранные минуты: {}\n'
+        '+++ броски с игры: {} из них результативных: {}\n'
+        '+++ точность бросков с игры: {:.1f} %\n'
+        '+++ 3-очковые броски: {} из них результативных: {}\n'
+        '+++ точность 3-очковых бросков: {:.1f} %\n'
+        '+++ штрафные броски: {} из них результативных: {}\n'
+        '+++ точность штрафных бросков: {:.1f} %\n'
+        '+++ подборы: {}, из них в нападении - {} и в защите - {}\n'
+        '+++ результативные передачи: {}\n'
+        '+++ перехваты: {}\n'
+        '+++ блоки: {}\n'
+        '+++ потери мяча: {}\n'
+        '+++ персональные замечания: {}\n'
+    ).format(
+        player_first_name, player_last_name,
+        player_id,
+        game_season, game_date,
+        'Команда 1', 'Команда 2', 
+        game_home_team_score, game_visitor_team_score,
+        pts, min, fga, fgm, fg_pct, fg3a,
+        fg3m, fg3_pct, fta, ftm, ft_pct,
+        reb, oreb, dreb, ast,
+        stl, blk, turnover, pf
+    )
+    return statistics_game_str
